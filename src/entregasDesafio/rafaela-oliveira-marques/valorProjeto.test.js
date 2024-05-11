64c9aac25c4ec32fad37929f263311f1b4bec572
RafaelaOMarques <rafaelao.marques@gmail.com>
2024-05-11 17:34:46 -0300

    Desafio Oficina Reprograma - Rafaela Oliveira Marques

--- src/entregasDesafio/rafaela-oliveira-marques/valorProjeto.test.js
+++ src/entregasDesafio/rafaela-oliveira-marques/valorProjeto.test.js
@@ -7,3 +7,5 @@ test("deve retornar 300 quando o valor da horas for 30 reais e o total de horas 
   const retornado = calcularValorBaseProjeto(100, 30);
   expect(esperado).toBe(retornado);
 });
+
+//Testando
