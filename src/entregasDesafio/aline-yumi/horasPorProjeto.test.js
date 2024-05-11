66fc590811f912f15fe18a3b2e981b2d53af91bd
yumi-br <132803304+yumi-br@users.noreply.github.com>
2024-05-11 17:37:56 -0300

    adiciona todos os testes

---
+++ src/entregasDesafio/aline-yumi/horasPorProjeto.test.js
@@ -0,0 +1,28 @@
+const {
+  calcularHorasDeProjeto,
+} = require("../../dominio/calculadora/Projeto/horasPorProjeto");
+
+describe("calcular horas de projeto", () => {
+  test("deve retornar 16 quando e apenas um formulário", () => {
+    const funcionalidades = ["formulario"];
+
+    const esperado = 16;
+    const retornado = calcularHorasDeProjeto(funcionalidades);
+
+    expect(esperado).toBe(retornado);
+  });
+
+  test("deve retornar 48 quando são 2 páginas com 1 formulário em cada", () => {
+    const funcionalidades = [
+      "formulario",
+      "formulario",
+      "construcao_1_pagina",
+      "construcao_1_pagina",
+    ];
+
+    const esperado = 48;
+    const retornado = calcularHorasDeProjeto(funcionalidades);
+
+    expect(esperado).toBe(retornado);
+  });
+});
