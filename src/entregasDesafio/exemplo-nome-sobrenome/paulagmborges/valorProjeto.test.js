c3f0b076cf0f76d1b83a292f48837b5dae874b16
paula borges <paulabzambeli@gmail.com>
2024-05-11 16:40:50 -0300

    desfaio-andamento

--- src/entregasDesafio/exemplo-nome-sobrenome/paulagmborges/valorProjeto.test.js
+++ src/entregasDesafio/exemplo-nome-sobrenome/paulagmborges/valorProjeto.test.js
@@ -6,12 +6,19 @@ const pacote = require("../../../dominio/calculadora/Projeto/pacote")
 jest.mock("../../../dominio/calculadora/Projeto/pacote")
 
 describe('calcular valor total do projeto', () =>{
- test("deve retornar 1584 quanodo o valor ora for 30 reais", () =>{
-    pacote.calcularPacote.mockReturnValue("pacote_intermediario")
-    const funcionalidades =["setup","formulario","responsabilidade","construcao_1_pagina"]
+    beforeEach(()=>{
+        pacote.calcularPacote.mockReturnValue("pacote_intermediario")
+    })
+ test("deve retornar 1613 quando o valor hora for 30 reais", () => {
+    
+    const funcionalidades =[
+    "setup",
+    "formulario",
+    "responsabilidade",
+    "construcao_1_pagina"]
     const valorHora = 30
     
-    const esperado = 1584
+    const esperado = 1613
     const retornado= calcularValorTotalProjeto(funcionalidades, valorHora)
  expect (esperado).toBe(retornado)
 })
